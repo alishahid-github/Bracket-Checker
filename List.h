33d3f7decec213741447fe6fd7c92f39f24e935a
Ali Shahid <71405228+alishahid-github@users.noreply.github.com>
2021-01-18 08:17:04 +0500

    Update List.h

--- List.h
+++ List.h
@@ -1,4 +1,4 @@
-//Header file for the Linked List with Basic funtions
+//Header file for the Linked List with Basic funtions use in Bracket Checker
 
 #include<iostream>
 #include"Node.h"
@@ -17,7 +17,6 @@ public:
 
 private:
 	Node* head;
-
 };
 
 #endif
